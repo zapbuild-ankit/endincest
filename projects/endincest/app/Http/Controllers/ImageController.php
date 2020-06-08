497d8a0af8d5bf671ddca9e5a01d63fee5895a7e
zapbuild-ankit <fbg.ankit@gmail.com>
2020-06-09 00:04:51 +0530

    image controller

---
+++ projects/endincest/app/Http/Controllers/ImageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Image;
+use Illuminate\Http\Request;
+
+class ImageController extends Controller {
+
+	// Method for image select with crop option
+
+	public function index() {
+		return view('image.image_crop');
+
+	}
+
+	// Method to upload cropped image
+
+	function upload(Request $request) {
+		if ($request->ajax()) {
+
+			$image_data = $request->image;
+
+			$image_array_1 = explode(";", $image_data);
+
+			$image_array_2 = explode(",", $image_array_1[1]);
+			$data          = base64_decode($image_array_2[1]);
+			$image_name    = time().'.jpeg';
+			$upload_path   = public_path('crop_image/'.$image_name);
+			file_put_contents($upload_path, $data);
+			$image        = new Image;
+			$image->image = $image_name;
+			$image->save();
+
+			return response()->json(['path' => '/crop_image/'.$image_name]);
+		}
+
+	}
+
+}
