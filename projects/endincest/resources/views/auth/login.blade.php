6c46e543674c501111d295656581898445d2db95
zapbuild-ankit <fbg.ankit@gmail.com>
2020-06-10 01:11:23 +0530

    modified login view file

--- projects/endincest/resources/views/auth/login.blade.php
+++ projects/endincest/resources/views/auth/login.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'></script>
-    <script src='http://parsleyjs.org/dist/parsley.js'></script>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -9,14 +7,14 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" id='form'>
+                    <form method="POST" action="{{ route('login') }}" id='login_form'>
                         @csrf
 
                         <div class="form-group">
                             <label for="email">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required data-parsley-trigger='keyup'>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -30,7 +28,7 @@
                             <label for="password">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required data-parsley-length="[8,16] "data-parsley-trigger='keyup'>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -54,15 +52,23 @@
 
                         <div class="form-group ">
                             <div class="col-md-8 ">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-success">
                                     {{ __('Login') }}
                                 </button>
+
+
+                                    <a href="{{route('fblogin')}}" class="btn btn-primary "><i class="fa fa-facebook-square"></i></a>
+
+                                    <a href="{{route('googlelogin')}}" class="btn btn-primary "><i class="fa fa-google"></i></a>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
+
+
+
                             </div>
                         </div>
                     </form>
@@ -72,6 +78,4 @@
     </div>
 </div>
 
-
-<script src="{{ asset('js/validation.js')}}"></script>
 @endsection
