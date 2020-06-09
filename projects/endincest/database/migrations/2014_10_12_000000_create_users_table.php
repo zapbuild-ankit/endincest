b24915e8f13544bf1a95de06a6add9137bc3b5d8
zapbuild-ankit <fbg.ankit@gmail.com>
2020-06-10 00:54:05 +0530

    users table migration modified

--- projects/endincest/database/migrations/2014_10_12_000000_create_users_table.php
+++ projects/endincest/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,10 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function (Blueprint $table) {
 				$table->bigIncrements('id');
 				$table->string('name');
-				$table->string('email')->unique();
+				$table->string('email')->unique()->nullable();
 				$table->timestamp('email_verified_at')->nullable();
 				$table->string('password');
-				$table->string('profile_pic');
+				$table->string('profile_pic')->nullable();
 				$table->rememberToken();
 				$table->timestamps();
 			});
