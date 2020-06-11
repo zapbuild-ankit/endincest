a2d24da86eec35da181b4b73b212d45671cdd177
zapbuild-ankit <fbg.ankit@gmail.com>
2020-06-11 15:17:30 +0530

    modified login controller

--- projects/endincest/app/Http/Controllers/Auth/LoginController.php
+++ projects/endincest/app/Http/Controllers/Auth/LoginController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-
 use App\SocialAccount;
 use App\User;
 use Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Socialite;
 
 class LoginController extends Controller {
@@ -124,4 +124,59 @@ class LoginController extends Controller {
 		return redirect('/home');
 
 	}
+
+	public function googleLogin(Request $request) {
+		$google_redirect_url = route('glogin');
+		$gClient             = new \Google_Client();
+		$gClient->setApplicationName(config('services.google.app_name'));
+		$gClient->setClientId(config('services.google.client_id'));
+		$gClient->setClientSecret(config('services.google.client_secret'));
+		$gClient->setRedirectUri($google_redirect_url);
+		$gClient->setDeveloperKey(config('services.google.api_key'));
+		$gClient->setScopes(array(
+				'https://www.googleapis.com/auth/plus.me',
+				'https://www.googleapis.com/auth/userinfo.email',
+				'https://www.googleapis.com/auth/userinfo.profile',
+			));
+		$google_oauthV2 = new \Google_Service_Oauth2($gClient);
+		if ($request->get('code')) {
+			$gClient->authenticate($request->get('code'));
+			$request->session()->put('token', $gClient->getAccessToken());
+		}
+		if ($request->session()->get('token')) {
+			$gClient->setAccessToken($request->session()->get('token'));
+		}
+		if ($gClient->getAccessToken()) {
+			//For logged in user, get details from google using access token
+			$guser = $google_oauthV2->userinfo->get();
+
+			$request->session()->put('name', $guser['name']);
+			if ($user = User::where('email', $guser['email'])->first()) {
+				//logged your user via auth login
+				Auth::login($user);
+
+				return redirect('/home');
+
+			} else {
+				//register your user with response data
+
+				$user = User::create([
+						'name'     => $guser->name,
+						'email'    => $guser->email,
+						'password' => md5(rand(1, 10000)),
+					]);
+
+				Auth::login($user);
+
+				return redirect('/home');
+
+			}
+
+		} else {
+			//For Guest user, get google login url
+			$authUrl = $gClient->createAuthUrl();
+			return redirect()->to($authUrl);
+		}
+	}
+
 }
