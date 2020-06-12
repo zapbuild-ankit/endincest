0f1b4e775995df5aa0876dc15f2a4b14ff26d82b
zapbuild-ankit <fbg.ankit@gmail.com>
2020-06-12 09:37:26 +0530

    modified image controller

--- projects/endincest/app/Http/Controllers/ImageController.php
+++ projects/endincest/app/Http/Controllers/ImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Image;
 use Illuminate\Http\Request;
+use Validator;
 
 class ImageController extends Controller {
 
@@ -16,24 +17,34 @@ class ImageController extends Controller {
 	// Method to upload cropped image
 
 	function upload(Request $request) {
-		if ($request->ajax()) {
 
-			$image_data = $request->image;
+		$validator = Validator::make($request->all(), [
+				'Image' => 'required',
+			]);
 
-			$image_array_1 = explode(";", $image_data);
+		if ($validator->passes()) {
 
-			$image_array_2 = explode(",", $image_array_1[1]);
-			$data          = base64_decode($image_array_2[1]);
-			$image_name    = time().'.jpeg';
-			$upload_path   = public_path('crop_image/'.$image_name);
-			file_put_contents($upload_path, $data);
-			$image        = new Image;
-			$image->image = $image_name;
-			$image->save();
+			if ($request->ajax()) {
 
-			return response()->json(['path' => '/crop_image/'.$image_name]);
+				$image_data = $request->image;
+
+				$image_array_1 = explode(";", $image_data);
+
+				$image_array_2 = explode(",", $image_array_1[1]);
+				$data          = base64_decode($image_array_2[1]);
+				$image_name    = time().'.jpeg';
+				$upload_path   = public_path('crop_image/'.$image_name);
+				file_put_contents($upload_path, $data);
+				$image        = new Image;
+				$image->image = $image_name;
+				$image->save();
+
+				return response()->json(['path' => '/crop_image/'.$image_name]);
+			}
+
 		}
 
+		return response()->json(['error' => $validator->errors()->all()]);
 	}
 
 }
