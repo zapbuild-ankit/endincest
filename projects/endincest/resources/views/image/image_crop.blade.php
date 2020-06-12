e856d23f6a4e2a2ec676a2e4322f296bdeae85a4
zapbuild-ankit <fbg.ankit@gmail.com>
2020-06-12 09:36:02 +0530

    modified crop image blade

--- projects/endincest/resources/views/image/image_crop.blade.php
+++ projects/endincest/resources/views/image/image_crop.blade.php
@@ -8,18 +8,24 @@
       <br />
       <div class="card">
         <div class="card-header">Crop and Upload Image</div>
-        <div class="card-body">
-            <form method="post" id="upload_form" enctype="multipart/form-data">
+        <div class="card-body"><br/>
+
+    <div class="alert alert-danger print-error-msg" style="display:none">
+        <ul></ul>
+    </div>
+
+            <form method="POST" id="crop_form" action ="upload" enctype="multipart/form-data">
 
           <div class="form-group">
                @csrf
+
             <div class="row">
               <div class="col-md-4" style="border-right:1px solid #ddd;">
                 <div id="image-preview"></div>
               </div>
               <div class="col-md-4" style="padding:75px; border-right:1px solid #ddd;">
                 <p><label>Select Image</label></p>
-                <input type="file" name="image" id="upload_image"  />
+                <input type="file" name="Image" id="upload_image"  />
                 <br />
                 <br />
                <input type="submit" name="upload" class="btn btn-success crop_image">
@@ -31,7 +37,7 @@
                 <div id="uploaded_image" align="center"></div>
               </div>
               <div>
-              	<b><p id="saved_image" style='color:blue;'></p></b>
+                 <b><p id="saved_image" style="color:green;" ></p></b>
               </div>
             </div>
           </div>
@@ -64,7 +70,7 @@ $(document).ready(function(){
 
   $('#upload_image').change(function(){
 
-
+$('.print-error-msg').remove();
 
     var reader = new FileReader();
 
@@ -77,41 +83,70 @@ $(document).ready(function(){
     reader.readAsDataURL(this.files[0]);
   });
 
-  $('.crop_image').click(function(event){
-  	 event.preventDefault();
+  $('.crop_image').click(function(e){
+ e.preventDefault();
 
-  	if ($("#upload_image").val() == "") {
-    alert('Please select image file');
-  }
-  else
+
     $image_crop.croppie('result', {
       type:'canvas',
       size:'viewport'
     }).then(function(response){
       var _token = $('input[name=_token]').val();
+      var Image = $('input[name=Image]').val();
+
+
       $.ajax({
         url:'{{ route("upload") }}',
         method:'POST',
 
-        data:{"image":response, _token:_token},
+        data:{"image":response,"Image":Image, _token:_token},
         dataType:"json",
 
 
         success:function(data)
         {
+if($.isEmptyObject(data.error)){
+    $('.print-error-msg').remove();
 
           var crop_image = '<img src="'+data.path+'" />';
           $('#uploaded_image').html(crop_image);
 
-          var saved_image = ' Image has been saved ';
+          var saved_image = ' Image Cropped and Saved Successfully ';
           $('#saved_image').html(saved_image);
+        }
+        else{
+                        printErrorMsg(data.error);
+                    }
         }
 
 
 
       });
+
+
     });
-  });
+
+
+
+});
+
+
+
+  function printErrorMsg (msg) {
+
+            $(".print-error-msg").find("ul").html('');
+
+            $(".print-error-msg").css('display','block');
+
+            $.each( msg, function( key, value ) {
+
+                $(".print-error-msg").find("ul").append('<li>'+value+'</li>');
+
+            });
+
+        }
+
+
 
 
 });
