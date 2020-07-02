0755863d0f38b17ed275fe84fc12cb07c055bea5
zapbuild-ankit <fbg.ankit@gmail.com>
2020-07-02 09:53:57 +0530

    paypal controller

--- projects/endincest/app/Http/Controllers/PaypalController.php
+++ projects/endincest/app/Http/Controllers/PaypalController.php
@@ -2,18 +2,27 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+
 use Srmklive\PayPal\Services\ExpressCheckout;
 
 class PaypalController extends Controller {
 
+	protected $provider;
+	public function __construct() {
+		$this->provider = new ExpressCheckout();
+
+	}
+
 	//Method to view order page
 
 	public function index() {
+
 		return view('paypal.paypal');
 	}
 	// Method  for payment process with paypal
 
 	public function payment(Request $request) {
+
 		$amount = $request->validate([
 				'amount' => 'required|numeric',
 			]);
@@ -23,58 +32,48 @@ class PaypalController extends Controller {
 			[
 				'name'  => 'endincest',
 				'price' => $amount,
-				'desc'  => '',
+				'desc'  => 'Description of endincest',
 				'qty'   => 1
 			]
 		];
+		$invoice_id = rand();
 
-		$data['invoice_id']          = 1;
+		$data['invoice_id']          = $invoice_id;
 		$data['invoice_description'] = "Order #{$data['invoice_id']} Invoice";
 		$data['return_url']          = route('payment.success');
 		$data['cancel_url']          = route('payment.cancel');
 		$data['total']               = $amount;
 
-		$provider = new ExpressCheckout;
+		//$provider = new ExpressCheckout;
 
-		$response = $provider->setExpressCheckout($data);
+		$response = $this->provider->setExpressCheckout($data);
 
-		$response = $provider->setExpressCheckout($data, true);
+		$response = $this->provider->setExpressCheckout($data, true);
 
 		return redirect($response['paypal_link']);
 	}
 
-	/**
-	 * Responds with a welcome message with instructions
-	 *
-	 * @return \Illuminate\Http\Response
-
-	 */
-
 	//Method to cancel payment
 
 	public function cancel() {
-		return redirect('/order')->with(['success' => "Your Payment is successfully canceled"]);
+
+		return redirect('/order')->with(['error' => "Your Payment is successfully canceled"]);
 
 	}
-
-	/**
-	 * Responds with a welcome message with instructions
-	 *
-	 * @return \Illuminate\Http\Response
-
-	 */
 
 	//Method for succcess transaction
 
 	public function success(Request $request) {
-		$response = $provider->getExpressCheckoutDetails($request->token);
+		$token = $this->provider->getExpressCheckoutDetails($request->token);
 
-		if (in_array(strtoupper($response['ACK']), ['SUCCESS', 'SUCCESSWITHWARNING'])) {
+		$response = $this->provider->getExpressCheckoutDetails($request->token);
+
+		if (in_array(strtoupper($response['ACK']), ['SUCCESS', 'SUCCESS'])) {
 
 			return redirect('/order')->with(['success' => "Your Payment is successfully done"]);
 
 		}
-		return redirect('/order')->with(['success' => "Something is wrong with your paymaent"]);
+		return redirect('/order')->with(['error' => "Something is wrong with your paymaent"]);
 
 	}
 
