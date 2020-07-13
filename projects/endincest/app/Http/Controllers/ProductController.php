4cfaf701b03f31a491ba00407758035e9eddd208
zapbuild-ankit <fbg.ankit@gmail.com>
2020-07-13 11:54:18 +0530

    Product controller

--- projects/endincest/app/Http/Controllers/ProductController.php
+++ projects/endincest/app/Http/Controllers/ProductController.php
@@ -9,8 +9,15 @@ use App\Wishlist;
 use Auth;
 use Illuminate\Http\Request;
 use Image;
+use Srmklive\PayPal\Services\ExpressCheckout;
 
 class ProductController extends Controller {
+
+	protected $provider;
+	public function __construct() {
+		$this->provider = new ExpressCheckout();
+
+	}
 
 	/**
 	 * Display a listing of the resource.
@@ -151,27 +158,24 @@ class ProductController extends Controller {
 			$wishlists = Wishlist::where('user_id', $user_id)->get();
 			$count     = 0;
 			$number    = 0;
-			if (isset($carts)) {
+			if (!$carts->isEmpty()) {
+
 				foreach ($carts as $cart) {
 					$product_id     = $cart->product_id;
 					$cart_product[] = Product::findOrFail($product_id);
 					$count          = $count+1;
+					$ids[]          = $product_id;
 				}
 
+				$questions = Product::where(function ($q) use ($ids) {
+						foreach ($ids as $key => $value) {
+							$key = 'id';
+							$q->where($key, '!=', $value);
+						}
+					})->get();
 			}
 
-			if (isset($cart_product)) {
-
-				foreach ($cart_product as $cart_products) {
-
-					$cart_products->status = 0;
-					$cart_products->save();
-
-				}
-
-			}
-
-			if (isset($wishlists)) {
+			if (!$wishlists->isEmpty()) {
 				foreach ($wishlists as $wishlist) {
 
 					$number = $number+1;
@@ -179,17 +183,16 @@ class ProductController extends Controller {
 
 			}
 
-			$products = Product::all();
-			if ($carts->isEmpty() && !$products->isEmpty()) {
-				foreach ($products as $product) {
-					$product->status = 1;
-					$product->save();
-				}
-			}
 		}
-		$products = Product::all();
 
-		return view('products.productview', compact('products', 'user', 'carts', 'count', 'number', 'cart_product'));
+		$products = Product::orderBy('created_at', 'DESC')->paginate(6);
+
+		$Allproducts = Product::all();
+		if (!$Allproducts->isEmpty()) {
+			$Allproducts_count = $Allproducts->count();
+		}
+
+		return view('products.productview', compact('products', 'user', 'carts', 'count', 'number', 'cart_product', 'Allproducts_count', 'questions'));
 	}
 
 	//Method to add products to cart
@@ -208,12 +211,7 @@ class ProductController extends Controller {
 					$cart->update();
 					return redirect('/cart');
 				}
-				if (isset($products)) {
-					foreach ($products as $product) {
-						$product->status = 0;
-						$product->save();
-					}
-				}
+
 			}
 		}
 		$carts = new Cart;
@@ -334,12 +332,6 @@ class ProductController extends Controller {
 			$cart->delete();
 		}
 
-		foreach ($products as $product) {
-
-			$product->status = 1;
-			$product->save();
-		}
-
 		return redirect('/cart');
 	}
 
@@ -357,6 +349,120 @@ class ProductController extends Controller {
 		}
 
 		return redirect('/wishlist');
+	}
+
+	//Method to filter products
+
+	public function search(Request $request) {
+
+		$query = $request->input('query');
+		$user  = Auth::User();
+
+		if ($user) {
+			$user_id   = Auth::User()->id;
+			$carts     = Cart::where('user_id', $user_id)->get();
+			$wishlists = Wishlist::where('user_id', $user_id)->get();
+			$count     = 0;
+			$number    = 0;
+			if (!$carts->isEmpty()) {
+				foreach ($carts as $cart) {
+					$product_id     = $cart->product_id;
+					$cart_product[] = Product::findOrFail($product_id);
+					$count          = $count+1;
+					$ids[]          = $product_id;
+				}
+
+				$questions = Product::where(function ($q) use ($ids) {
+						foreach ($ids as $key => $value) {
+							$key = 'id';
+							$q->where($key, '!=', $value);
+						}
+					})->get();
+			}
+
+			if (isset($wishlists)) {
+				foreach ($wishlists as $wishlist) {
+
+					$number = $number+1;
+				}
+
+			}
+
+		}
+
+		$productlist = Product::all();
+		if (!$productlist->isEmpty()) {
+			$productcount = $productlist->count();
+
+			$products = Product::where('name', 'like', "%$query%")
+				->orWhere('price', 'like', "%$query%")
+				->orWhere('category', 'like', "%$query%")
+				->orWhere('description', 'like', "%$query%")
+				->paginate($productcount);
+
+		}
+
+		return view('products.search_result', compact('products', 'user', 'carts', 'count', 'number', 'cart_product', 'questions'));
+	}
+
+	//Method for payment with  paypal
+
+	public function paypal_payment($id) {
+
+		$product = Product::findOrFail($id);
+
+		if (isset($product)) {
+
+			$product_price = $product->price;
+
+			$data          = [];
+			$data['items'] = [
+				[
+					'name'  => 'endincest',
+					'price' => $product_price,
+					'desc'  => 'Description of endincest',
+					'qty'   => 1
+				]
+			];
+			$invoice_id = rand();
+
+			$data['invoice_id']          = $invoice_id;
+			$data['invoice_description'] = "Order #{$data['invoice_id']} Invoice";
+			$data['return_url']          = route('payment.success');
+			$data['cancel_url']          = route('payment.cancel');
+			$data['total']               = $product_price;
+
+			$response = $this->provider->setExpressCheckout($data);
+
+			$response = $this->provider->setExpressCheckout($data, true);
+
+			return redirect($response['paypal_link']);
+		}
+
+	}
+
+	//Method to cancel payment
+
+	public function cancel() {
+
+		return redirect()->back()->with(['error' => "Your Payment is successfully canceled"]);
+
+	}
+
+	//Method for succcess transaction
+
+	public function success(Request $request) {
+		$token = $this->provider->getExpressCheckoutDetails($request->token);
+
+		$response = $this->provider->getExpressCheckoutDetails($request->token);
+
+		if (in_array(strtoupper($response['ACK']), ['SUCCESS', 'SUCCESS'])) {
+
+			return redirect()->back()->with(['success' => "Your Payment is successfully done"]);
+
+		}
+		return redirect()->back()->with(['error' => "Something is wrong with your paymaent"]);
+
 	}
 
 }
