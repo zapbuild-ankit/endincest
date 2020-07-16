435d173c0bb15ada5b09c479043e7960104806a9
zapbuild-ankit <fbg.ankit@gmail.com>
2020-07-16 11:28:13 +0530

    database seeder file

--- projects/endincest/database/seeds/DatabaseSeeder.php
+++ projects/endincest/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call(RolesTableSeeder::class );
 		$this->call(UsersTableSeeder::class );
 		$this->call(LocationsTableSeeder::class );
+		$this->call(AdminsTableSeeder::class );
 
 	}
 }
