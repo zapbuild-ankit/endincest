478283b60a02725e88812deb110a549948fcd756
zapbuild-ankit <fbg.ankit@gmail.com>
2020-07-16 10:37:40 +0530

    Handler file modified

--- projects/endincest/app/Exceptions/Handler.php
+++ projects/endincest/app/Exceptions/Handler.php
@@ -3,49 +3,63 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
-class Handler extends ExceptionHandler
-{
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+class Handler extends ExceptionHandler {
+	/**
+	 * A list of the exception types that are not reported.
+	 *
+	 * @var array
+	 */
+	protected $dontReport = [
+		//
+	];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
+	/**
+	 * A list of the inputs that are never flashed for validation exceptions.
+	 *
+	 * @var array
+	 */
+	protected $dontFlash = [
+		'password',
+		'password_confirmation',
+	];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
+	/**
+	 * Report or log an exception.
+	 *
+	 * @param  \Exception  $exception
+	 * @return void
+	 */
+	public function report(Exception $exception) {
+		parent::report($exception);
+	}
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
-    }
+	/**
+	 * Render an exception into an HTTP response.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Exception  $exception
+	 * @return \Illuminate\Http\Response
+	 */
+	public function render($request, Exception $exception) {
+		return parent::render($request, $exception);
+	}
+
+	protected function unauthenticated($request, AuthenticationException $exception) {
+		if (in_array('admin', $exception->guards())) {
+			return $request->expectsJson()
+			?response()    ->json([
+					'message' => $exception->getMessage()
+				], 401)
+			:redirect()->guest(route('admin.login'));
+		}
+
+		return $request->expectsJson()
+		?response()    ->json([
+				'message' => $exception->getMessage()
+			], 401)
+		:redirect()->guest(route('login'));
+	}
 }
