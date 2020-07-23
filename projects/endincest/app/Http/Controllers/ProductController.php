a0dd76bb2941f4cf247c75bc811f248631550c67
zapbuild-ankit <fbg.ankit@gmail.com>
2020-07-23 10:38:42 +0530

    Product controller modified

--- projects/endincest/app/Http/Controllers/ProductController.php
+++ projects/endincest/app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Cart;
+use App\Coupon;
+use App\Order;
 use App\Product;
 use App\User;
 use App\Wishlist;
@@ -279,39 +281,90 @@ class ProductController extends Controller {
 		return view('products.search_result', compact('products', 'user', 'carts', 'count', 'number', 'cart_product', 'questions'));
 	}
 
+	//Method to show checkout page
+
+	public function checkout_form($id) {
+		$product = Product::findOrFail($id);
+		$user    = Auth::User();
+		$coupons = Coupon::all();
+
+		if ($user) {
+
+			$user_id = Auth::user()->id;
+
+			$wishlists = Wishlist::where('user_id', $user_id)->get();
+			$carts     = Cart::where('user_id', $user_id)->get();
+			$count     = 0;
+			$number    = 0;
+			if (!$carts->isEmpty()) {
+				foreach ($carts as $cart) {
+					$count = $count+1;
+				}
+			}
+			if (!$wishlists->isEmpty()) {
+				foreach ($wishlists as $wishlist) {
+					$number = $number+1;
+
+				}
+			}
+
+		}
+
+		return view('products.checkout_form', compact('product', 'number', 'count', 'coupons'));
+	}
+
 	//Method for payment with  paypal
 
-	public function paypal_payment($id) {
+	public function paypal_payment(Request $request, $id) {
 
-		$product = Product::findOrFail($id);
+		$validatedData = $request->validate([
+				'name'    => 'required',
+				'street1' => 'required',
+				'city'    => 'required',
+				'phone'   => 'required',
+				'state'   => 'required',
+				'zip'     => 'required',
 
-		if (isset($product)) {
+			]);
+
+		session()->put('address', [
+				'name' => $request->name, 'phone' => $request->phone, 'street1' => $request->street1, 'street2' => $request->street2, 'city' => $request->city, 'state' => $request->state, 'zip' => $request->zip, ]);
+
+		if (session()->has('coupon') && $id == session()->get('coupon')['id']) {
+			$product_price = session()->get('coupon')['new_total'];
+		}
+		if (!session()->has('coupon') || $id != session()->get('coupon')['id']) {
+			$product = Product::findOrFail($id);
 
 			$product_price = $product->price;
-
-			$data          = [];
-			$data['items'] = [
-				[
-					'name'  => 'endincest',
-					'price' => $product_price,
-					'desc'  => 'Description of endincest',
-					'qty'   => 1
-				]
-			];
-			$invoice_id = rand();
-
-			$data['invoice_id']          = $invoice_id;
-			$data['invoice_description'] = "Order #{$data['invoice_id']} Invoice";
-			$data['return_url']          = route('payment.success');
-			$data['cancel_url']          = route('payment.cancel');
-			$data['total']               = $product_price;
-
-			$response = $this->provider->setExpressCheckout($data);
-
-			$response = $this->provider->setExpressCheckout($data, true);
-
-			return redirect($response['paypal_link']);
+			session()->put('price', ['total' => $product_price]);
 		}
+
+		$data          = [];
+		$data['items'] = [
+			[
+				'name'  => 'endincest',
+				'price' => $product_price,
+				'desc'  => 'Description of endincest',
+				'qty'   => 1,
+
+			]
+		];
+		$invoice_id = rand();
+
+		$data['invoice_id']          = $invoice_id;
+		$data['invoice_description'] = "Order #{$data['invoice_id']} Invoice";
+		$data['return_url']          = route('payment.success');
+		$data['cancel_url']          = route('payment.cancel');
+		$data['total']               = $product_price;
+
+		$response = $this->provider->setExpressCheckout($data);
+
+		$response = $this->provider->setExpressCheckout($data, true);
+		session()->put('product', [
+				'id' => $id]);
+
+		return redirect($response['paypal_link']);
 
 	}
 
@@ -332,11 +385,115 @@ class ProductController extends Controller {
 
 		if (in_array(strtoupper($response['ACK']), ['SUCCESS', 'SUCCESS'])) {
 
-			return redirect()->back()->with(['success' => "Your Payment is successfully done"]);
+			$user_id = Auth::User()->id;
+
+			$order = new Order;
+
+			if (session()->has('coupon') && session()->get('product')['id'] == session()->get('coupon')['id']) {
+
+				$order->coupon_code   = session()->get('coupon')['name'];
+				$order->discount_type = session()->get('coupon')['type'];
+				$order->discount      = session()->get('coupon')['discount'];
+				$order->total         = session()->get('coupon')['new_total'];
+
+			}
+			if (!session()->has('coupon') || session()->get('product')['id'] != session()->get('coupon')['id']) {
+				$order->total = session()->get('price')['total'];
+			}
+
+			$order->user_id         = $user_id;
+			$order->payment_method  = 'online';
+			$order->payment_geteway = 'paypal';
+			$order->product_id      = session()->get('product')['id'];
+			$order->name            = session()->get('address')['name'];
+			$order->phone           = session()->get('address')['phone'];
+			$order->street1         = session()->get('address')['street1'];
+			$order->street2         = session()->get('address')['street2'];
+			$order->city            = session()->get('address')['city'];
+			$order->state           = session()->get('address')['state'];
+			$order->zip             = session()->get('address')['zip'];
+			$order->save();
+
+			session()->forget('coupon');
+
+			$carts = Cart::select('*')->where([
+					['product_id', '=', session()->get('product')['id']], ['user_id', '=', $user_id]
+				])->get();
+
+			$wishlists = Wishlist::select('*')->where([
+					['product_id', '=', session()->get('product')['id']], ['user_id', '=', $user_id]
+				])->get();
+			if (!$carts->isEmpty()) {
+				foreach ($carts as $cart) {
+
+					$cart->delete();
+				}
+
+			}
+
+			if (!$wishlists->isEmpty()) {
+				foreach ($wishlists as $wishlist) {
+
+					$wishlist->delete();
+				}
+
+			}
+
+			return redirect('product_view')->with(['success' => "Your Payment is successfully done"]);
 
 		}
 		return redirect()->back()->with(['error' => "Something is wrong with your paymaent"]);
 
+	}
+
+	//Method to match coupon code  with Available coupon code
+
+	public function coupon_check(Request $request, $id) {
+		$coupon = Coupon::where('code', $request->coupon_code)->first();
+
+		if (!$coupon) {
+			return back()->withErrors('Invalid coupon code. Please try again.');
+		}
+
+		$user_id = Auth::User()->id;
+
+		$order = Order::where([
+				['coupon_code', '=', $request->coupon_code], ['user_id', '=', $user_id]])->get();
+
+		if (!$order->isEmpty()) {
+
+			return back()->withErrors('Coupon has been used before ');
+
+		}
+
+		$product = Product::findOrFail($id);
+		$price   = $product->price;
+		$type    = $coupon->type;
+		if ($type == 'Fixed') {
+			$new_total = $price-($coupon->value);
+			$discount  = $coupon->value;
+
+		} elseif ($type == 'Percent') {
+			$discount  = $coupon->percent_off;
+			$new_total = $price-round(($coupon->percent_off/100)*$price);
+		} else {
+			$discount  = 0;
+			$new_total = $price;
+		}
+		session()->put('coupon', [
+				'name'      => $coupon->code,
+				'discount'  => $discount,
+				'new_total' => $new_total,
+				'type'      => $type,
+				'id'        => $id]);
+		return back()->with('success', 'Coupon has been applied!');
+	}
+
+	//Method to remove coupon
+	public function coupon_remove() {
+		session()->forget('coupon');
+
+		return back()->with('success', 'Coupon has been removed.');
 	}
 
 }
